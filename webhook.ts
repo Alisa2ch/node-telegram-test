52d939fcef8253080eb2f70bb3df8fed9bb6ef15
noname <foxandapples@gmail.com>
2023-05-29 11:57:55 +0300

    add webhook.ts

---
+++ webhook.ts
@@ -0,0 +1,29 @@
+import { Bot, webhookCallback } from 'grammy'
+import express from 'express'
+
+const bot = new Bot(process.env.TELEGRAM_TOKEN as string,{
+	client: {
+		   apiRoot: process.env.TELEGRAM_SERVER || '')
+		}
+	})
+
+bot.on('message:text', ctx => {
+	console.log(ctx);
+	ctx.reply(ctx.message.text);
+})
+
+const server = express()
+server.use(express.json())
+
+server.post('/webhook', webhookCallback(bot, 'express'))
+
+server.listen(process.env.TELEGRAM_WEBHOOK_PORT)
+
+bot.api.setWebhook(process.env.TELEGRAM_WEBHOOK_SERVER)
+
+console.log([
+	process.env.TELEGRAM_TOKEN,
+	process.env.TELEGRAM_SERVER,
+	process.env.TELEGRAM_WEBHOOK_PORT,
+	process.env.TELEGRAM_WEBHOOK_SERVER
+])
