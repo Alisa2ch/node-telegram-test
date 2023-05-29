1429960c526b24880610babeda22f8a794c63bd0
noname <foxandapples@gmail.com>
2023-05-29 12:36:52 +0300

    fix webhook.ts

--- webhook.ts
+++ webhook.ts
@@ -1,9 +1,9 @@
 import { Bot, webhookCallback } from 'grammy'
 import express from 'express'
 
-const bot = new Bot(process.env.TELEGRAM_TOKEN as string,{
+const bot = new Bot(process.env.TELEGRAM_TOKEN as string, {
 	client: {
-		   apiRoot: process.env.TELEGRAM_SERVER || '')
+		   apiRoot: process.env.TELEGRAM_SERVER || ''
 		}
 	})
 
@@ -19,7 +19,7 @@ server.post('/webhook', webhookCallback(bot, 'express'))
 
 server.listen(process.env.TELEGRAM_WEBHOOK_PORT)
 
-bot.api.setWebhook(process.env.TELEGRAM_WEBHOOK_SERVER)
+bot.api.setWebhook(process.env.TELEGRAM_WEBHOOK_SERVER as string)
 
 console.log([
 	process.env.TELEGRAM_TOKEN,
