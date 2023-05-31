37629bd5fa2d7068dfa8a67961241951dc2a7f80
noname <foxandapples@gmail.com>
2023-05-31 20:00:56 +0300

    fix lint

--- webhook.ts
+++ webhook.ts
@@ -1,29 +1,29 @@
-import { Bot, webhookCallback } from 'grammy'
-import express from 'express'
+import { Bot, webhookCallback } from 'grammy';
+import express from 'express';
 
 const bot = new Bot(process.env.TELEGRAM_TOKEN as string, {
 	client: {
-		   apiRoot: process.env.TELEGRAM_SERVER || ''
-		}
-	})
+		apiRoot: process.env.TELEGRAM_SERVER || ''
+	}
+});
 
 bot.on('message:text', ctx => {
 	console.log(ctx);
 	ctx.reply(ctx.message.text);
 })
 
-const server = express()
-server.use(express.json())
+const server = express();
+server.use(express.json());
 
-server.post('/webhook', webhookCallback(bot, 'express'))
+server.post('/webhook', webhookCallback(bot, 'express'));
 
-server.listen(process.env.TELEGRAM_WEBHOOK_PORT)
+server.listen(process.env.TELEGRAM_WEBHOOK_PORT);
 
-bot.api.setWebhook(process.env.TELEGRAM_WEBHOOK_SERVER as string)
+bot.api.setWebhook(process.env.TELEGRAM_WEBHOOK_SERVER as string);
 
 console.log([
 	process.env.TELEGRAM_TOKEN,
 	process.env.TELEGRAM_SERVER,
 	process.env.TELEGRAM_WEBHOOK_PORT,
 	process.env.TELEGRAM_WEBHOOK_SERVER
-])
+]);
