3f3a47048a7cb3f85aed05b4d666ac497bbc2593
noname <foxandapples@gmail.com>
2023-06-06 04:33:34 +0300

    fix: run create user after /start

--- webhook.ts
+++ webhook.ts
@@ -10,8 +10,8 @@ const bot = new Bot(process.env.TELEGRAM_TOKEN as string, {
 });
 
 bot.command('start', async (ctx) => {
-	console.log(ctx.hasOwnProperty('from'), ctx.from != null, ctx?.from?.hasOwnProperty('id'))
-	if(ctx.hasOwnProperty('from') && ctx.from != null && ctx.from.hasOwnProperty('id')){
+	console.log(ctx.from != null, ctx?.from?.hasOwnProperty('id'))
+	if(ctx.from != null && ctx.from.hasOwnProperty('id')){
 
 		const user = await prisma.user.create({
 			data: {
