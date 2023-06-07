421028cbfefe4421f2ec3c2fb613feb14c8e5d93
noname <foxandapples@gmail.com>
2023-06-07 16:36:57 +0300

    fix: add more arg in deploy.sh

--- prisma/deploy.sh
+++ prisma/deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+VER_MIGRATE0="0.0.1.$(shuf -i 0-100000 -n 1)"
 VER_MIGRATE="0.0.1.$(shuf -i 0-100000 -n 1)"
 echo $VER_MIGRATE
 mkdir -p migrations/$VER_MIGRATE
 npx prisma migrate diff \
 --from-empty \
+--script > migrations/VER_MIGRATE0/migration.sql
+npx prisma db pull
+npx prisma migrate resolve --applied $VER_MIGRATE0
+npx prisma migrate diff \
+--from-migrations migrations
+--shadow-database-url $SHADOW_DATABASE_URL
 --to-schema-datamodel schema.prisma \
 --script > migrations/$VER_MIGRATE/migration.sql
-npx prisma migrate resolve --applied $VER_MIGRATE
 npx prisma migrate deploy
