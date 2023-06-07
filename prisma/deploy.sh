b7e7a0dafa9b3dac93d247e77da486535e33b66f
noname <foxandapples@gmail.com>
2023-06-07 16:10:35 +0300

    add script for deploy db scheme

---
+++ prisma/deploy.sh
@@ -0,0 +1,8 @@
+VER_MIGRATE=0.0.1.$(shuf -i 0-100000 -n 1)
+mkdir -p migrations/$VER_MIGRATE
+npx prisma migrate diff \
+--from-empty \
+--to-schema-datamodel schema.prisma \
+--script > migrations/$VER_MIGRATE/migration.sql
+npx prisma migrate resolve --applied $VER_MIGRATE
+npx prisma migrate deploy
