09ca071a944f131e92ea04a67ae740b682a2440c
noname <foxandapples@gmail.com>
2023-06-17 14:07:02 +0300

    add in test conv load data from DB and validation answer

--- webhook.ts
+++ webhook.ts
@@ -15,7 +15,24 @@ const prisma = new PrismaClient();
 interface SessionData {
 }
 
-type customContext = Context & SessionFlavor<SessionData> & ConversationFlavor;
+const validations = {
+	date: (date: string): boolean => {
+		const reg = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/; 
+		return reg.test(date)
+	},
+	name: (name: string): boolean => {
+		const reg = /^[a-zA-Z]+$/; 
+		return reg.test(name);
+	},
+	companyId: (companyId: string): boolean => {
+		const reg = /^[A-Z][0-9]{4}$/;
+		return reg.test(companyId);
+	}
+}
+
+type customContext = Context & 
+SessionFlavor<SessionData> & 
+ConversationFlavor;
 type customConversation = Conversation<customContext>
 
 const bot = new Bot<customContext>(process.env.TELEGRAM_TOKEN as string, {
@@ -35,9 +52,32 @@ const initial = (): SessionData => {
 	return {};
 }
 
+const getAskForConv = async (name: string) => {
+	return await prisma.dataForConv.findMany({
+		where: {
+			name
+		}
+	});
+}
+
 const testConversation = async (conversation: customConversation, ctx: customContext) => {
-	await ctx.reply("Wellcome to conversation 'test'\nEnter your name");
-	const name = await conversation.form.text();
+	await ctx.reply("Wellcome to conversation 'test'\nEnter name ask");
+	const name: string = await conversation.form.text();
+	const asks = await conversation.external(() => getAskForConv(name));
+	for (let indexAsk = 0; indexAsk < asks.length; indexAsk++) {
+		const {ask, validate} = asks[indexAsk];
+		let checkValidate: boolean = !false;
+		while(checkValidate){
+			await ctx.reply(`${ask}`);
+			const answer: string = await conversation.form.text();
+			checkValidate = !validations[validate](answer)
+			if(!checkValidate){
+				await ctx.reply("OK");
+			}else{
+				await ctx.reply("Try again");
+			}
+		}
+	}
 	await ctx.reply(`Your name: ${name}`);
 }
 
